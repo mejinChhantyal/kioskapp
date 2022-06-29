f00155adff487ebb7dca27f83e3b09f8eb3f67bc
Cesar Amezquita Ramirez <cramirez@icstars.org>
2022-06-29 18:57:43 -0500

    Image directory
    Added the maps to the Directory, going to edit the images after but just want them there to have something for their place.

--- src/pages/Directory/Directory.js
+++ src/pages/Directory/Directory.js
@@ -2,6 +2,11 @@
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 import pic1 from './map1.png'
+import pic2 from './map2.png'
+import pic3 from './map3.png'
+import pic4 from './map4.png'
+import pic5 from './map5.png'
+import pic6 from './map6.png'
 
 
 const Directory = () => {
@@ -9,7 +14,11 @@ const Directory = () => {
     <div>
      <AliceCarousel autoPlay> 
         <img src= {pic1}  className= "map" ></img>
-        <img src= {pic1}  className= "map" ></img>
+        <img src= {pic2}  className= "map" ></img>
+        <img src= {pic3}  className= "map" ></img>
+        <img src= {pic4}  className= "map" ></img>
+        <img src= {pic5}  className= "map" ></img>
+        <img src= {pic6}  className= "map" ></img>
     </AliceCarousel>
     </div>
 
