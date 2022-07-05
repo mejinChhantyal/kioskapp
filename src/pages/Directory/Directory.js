57cc6d520e5781c410a36f5f75e4fa73ccb8830e
Cesar Amezquita Ramirez <cramirez@icstars.org>
2022-07-05 10:04:08 -0500

    Changes to the Directory
    Just added and deleted things I had and moved my images to the public folder

--- src/pages/Directory/Directory.js
+++ src/pages/Directory/Directory.js
@@ -1,22 +1,12 @@
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-import pic1 from './map1.png'
-import pic2 from './map2.png'
-import pic3 from './map3.png'
-import pic4 from './map4.png'
-import pic5 from './map5.png'
-import pic6 from './map6.png'
+import Glide from './Glide'
 
 
-const Directory = () => {
- return (
-    <div>
-     <AliceCarousel autoPlay> 
-        <img src= {pic1}  className= "map" alt='Floor 1'></img>
-        <img src= {pic1}  className= "map" alt='Floor 2'></img>
-    </AliceCarousel>
-    </div>
- )
-};
+function Directory() {
+  return (
+    <>
+      <Glide />
+    </>
+  );
+}
 
 export default Directory;
